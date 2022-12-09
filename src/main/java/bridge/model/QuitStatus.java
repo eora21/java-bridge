2d4eff91f31c7dde9b8224376fd8d380d4b4df59
eora21 <eora21@naver.com>
2022-12-09 17:51:49 +0900

    feat: 🎸 변경 종료를 원하는 TF를 내부에 포함하도록 수정

--- src/main/java/bridge/model/QuitStatus.java
+++ src/main/java/bridge/model/QuitStatus.java
@@ -3,22 +3,25 @@ package bridge.model;
 import java.util.Arrays;
 
 public enum QuitStatus {
-    QUIT("Q", "종료"),
-    RETRY("R", "재시도");
+    QUIT("Q", "종료", true),
+    RETRY("R", "재시도", false);
 
     private final String command;
     private final String description;
+    private final boolean status;
 
-    QuitStatus(String command, String description) {
+    QuitStatus(String command, String description, boolean status) {
         this.command = command;
         this.description = description;
+        this.status = status;
     }
 
-    public static QuitStatus findByCommand(String command) {
+    public static boolean findStatusByCommand(String command) {
         return Arrays.stream(QuitStatus.values())
                 .filter(quitStatus -> quitStatus.command.equals(command))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 커맨드입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 커맨드입니다."))
+                .status;
     }
 
     public String getCommand() {
