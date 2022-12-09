075adccb2f65e3acf164d411a7574cda1f124b0b
eora21 <eora21@naver.com>
2022-12-09 15:21:10 +0900

    feat: 🎸 추가 다리 목록을 지니는 일급 컬렉션

---
+++ src/main/java/bridge/model/Bridges.java
@@ -0,0 +1,22 @@
+package bridge.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bridges {
+    private final List<Bridge> bridges;
+
+    public Bridges(List<String> bridge) {
+        this.bridges = bridge.stream()
+                .map(Bridge::findBridgeByCommand)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isSafe(int turn, Bridge bridge) {
+        try {
+            return bridges.get(turn) == bridge;
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("다리의 범위를 벗어났습니다.");
+        }
+    }
+}
