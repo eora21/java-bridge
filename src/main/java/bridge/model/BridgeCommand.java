2191f75617a1549a0e820b08ab5154551160e46f
eora21 <eora21@naver.com>
2022-12-09 14:03:38 +0900

    feat: 🎸 추가 다리 선택 커맨드 enum 및 랜덤값으로 커맨드를 반환하는 메서드

---
+++ src/main/java/bridge/model/BridgeCommand.java
@@ -0,0 +1,25 @@
+package bridge.model;
+
+import java.util.Arrays;
+
+public enum BridgeCommand {
+    UP("U", 1),
+    DOWN("D", 0);
+
+    private final String command;
+
+    private final int randomValue;
+
+    BridgeCommand(String command, int randomValue) {
+        this.command = command;
+        this.randomValue = randomValue;
+    }
+
+    public String findCommandByRandomValue(int randomValue) {
+        return Arrays.stream(BridgeCommand.values())
+                .filter(bridge -> bridge.randomValue == randomValue)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 커맨드가 없습니다."))
+                .command;
+    }
+}
