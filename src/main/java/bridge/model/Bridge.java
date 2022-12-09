96c934671fbeaca2c0e23fed30c1e5aedceaa7e3
eora21 <eora21@naver.com>
2022-12-09 15:21:43 +0900

    refactor: 💡 변경 BridgeCommand 자체를 Bridge로 변경

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -2,24 +2,22 @@ package bridge.model;
 
 import java.util.Arrays;
 
-public enum BridgeCommand {
-    UP_COMMAND("U", 1, "위", Bridge.UP_BRIDGE),
-    DOWN_COMMAND("D", 0, "아래", Bridge.DOWN_BRIDGE);
+public enum Bridge {
+    UP("U", 1, "위"),
+    DOWN("D", 0, "아래");
 
     private final String command;
     private final int randomValue;
     private final String description;
-    private final Bridge bridge;
 
-    BridgeCommand(String command, int randomValue, String description, Bridge bridge) {
+    Bridge(String command, int randomValue, String description) {
         this.command = command;
         this.randomValue = randomValue;
         this.description = description;
-        this.bridge = bridge;
     }
 
     public static String findCommandByRandomValue(int randomValue) {
-        return Arrays.stream(BridgeCommand.values())
+        return Arrays.stream(Bridge.values())
                 .filter(bridgeCommand -> bridgeCommand.randomValue == randomValue)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 커맨드가 없습니다."))
@@ -27,11 +25,10 @@ public enum BridgeCommand {
     }
 
     public static Bridge findBridgeByCommand(String command) {
-        return Arrays.stream(BridgeCommand.values())
+        return Arrays.stream(Bridge.values())
                 .filter(bridgeCommand -> bridgeCommand.command.equals(command))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("커맨드와 일치하는 다리가 없습니다."))
-                .bridge;
+                .orElseThrow(() -> new IllegalArgumentException("커맨드와 일치하는 다리가 없습니다."));
     }
 
     public String getCommand() {
