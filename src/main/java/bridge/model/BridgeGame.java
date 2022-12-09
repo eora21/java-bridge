7bfba2fd5e3ca20e9a0cbe0348b5cc47d3f40ca6
eora21 <eora21@naver.com>
2022-12-09 16:45:01 +0900

    feat: 🎸 추가 다리 데이터와 로그 일급컬렉션 지닌 채 생성

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -5,6 +5,14 @@ package bridge.model;
  */
 public class BridgeGame {
 
+    private final Bridges bridges;
+    private final BridgeGameLog bridgeGameLog;
+
+    public BridgeGame(Bridges bridges, BridgeGameLog bridgeGameLog) {
+        this.bridges = bridges;
+        this.bridgeGameLog = bridgeGameLog;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
