64b259c7110ac6fff7cf9222c097614ec2d57914
eora21 <eora21@naver.com>
2022-12-09 17:00:23 +0900

    refactor: 💡 변경 Safe Enum 제거
    O, X는 어디까지나 출력에 의거한 데이터라 판단. O, X가 바뀌었을 때 출력쪽을 확인하고 해당 출력 규칙을 바꾸는 게 더 맞는
    것 같다.

--- src/main/java/bridge/model/Log.java
+++ src/main/java/bridge/model/Log.java
@@ -1,32 +1,32 @@
 package bridge.model;
 
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class Log {
-    enum Safe{O, X}
 
-    private static final Map<Bridge, Map<Safe, Log>> CACHE = new EnumMap<>(Bridge.class) {{
+    private static final Map<Bridge, Map<Boolean, Log>> CACHE = new EnumMap<>(Bridge.class) {{
         for (Bridge bridge : Bridge.values()) {
-            put(bridge, new EnumMap<>(Safe.class));
+            put(bridge, new HashMap<>());
         }
     }};
 
     private final Bridge bridge;
-    private final Safe safe;
+    private final boolean safe;
 
-    private Log(Bridge bridge, Safe safe) {
+    private Log(Bridge bridge, boolean safe) {
         this.bridge = bridge;
         this.safe = safe;
     }
 
-    public static Log getInstance(Bridge bridge, Safe safe) {
-        Log log = CACHE.get(bridge).get(safe);
+    public static Log getInstance(Bridge bridge, boolean isSafe) {
+        Log log = CACHE.get(bridge).get(isSafe);
 
         if (Objects.isNull(log)) {
-            log = new Log(bridge, safe);
-            CACHE.get(bridge).put(safe, log);
+            log = new Log(bridge, isSafe);
+            CACHE.get(bridge).put(isSafe, log);
         }
 
         return log;
@@ -36,7 +36,7 @@ public class Log {
         return bridge;
     }
 
-    public Safe getSafe() {
+    public boolean getSafe() {
         return safe;
     }
 }
