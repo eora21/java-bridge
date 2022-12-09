476d2c54d16ecebfc0491c6737bfb85d4b83e8f1
eora21 <eora21@naver.com>
2022-12-09 16:45:30 +0900

    feat: 🎸 추가 게임을 생성해 반환하는 서비스 메서드

---
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -0,0 +1,22 @@
+package bridge.service;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
+import bridge.model.BridgeGameLog;
+import bridge.model.Bridges;
+import java.util.List;
+
+public class BridgeGameService {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+
+    public BridgeGame createBridgeGame(int bridgeSize) {
+        List<String> bridgeCommands = bridgeMaker.makeBridge(bridgeSize);
+        Bridges bridges = new Bridges(bridgeCommands);
+        return new BridgeGame(bridges, new BridgeGameLog());
+    }
+}
